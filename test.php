941cb001255d77368f172576496021a58a38fbdf
the-customer <alytallniang@gmail.com>
2026-06-03 10:01:11 +0000

    adding exercise and the dashbord template

---
+++ test.php
@@ -0,0 +1,10 @@
+Recherche a faire:
+
+- tableau multidimensionnel ;
+- for / foreach ;
+- count() ;
+- incrémentation avec ++;
+- affichage dynamique dans un tableau HTML.
+
+a envoyer par whatsapp : 771825414 
+avant 9h35
